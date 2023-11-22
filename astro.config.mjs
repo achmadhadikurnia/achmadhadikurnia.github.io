e12827013a37f88a6ac5a34724503b52d4986592
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2023-11-22 16:04:18 +0700

    Configure @astrojs/vercel/serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://achmadhadikurnia.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+    adapter: vercel({
+      imageService: true,
+    }),
+  }),
 });
